d2b045df8c30788289d8ea506d1315b16fa7d09b
Shriram Patil <shrirampatil246@gmail.com>
2022-09-29 17:26:55 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,41 @@
+import java.rmi.StubNotFoundException;
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        double n1 = sr.nextDouble();
+        System.out.println("Enter Second Number:");
+        double n2 = sr.nextDouble();
+        System.out.print("Enter operator(+ , - , * , /):");
+        char operator = sr.next().charAt(0);
+        sr.close();
+        double output;
+
+        switch (operator) {
+            case '+':
+                output = n1 + n2;
+                break;
+
+            case '-':
+                output = n1 - n2;
+                break;
+
+            case '*':
+                output = n1 * n2;
+                break;
+
+            case '/':
+                output = n1 / n2;
+                break;
+
+            default:
+                System.out.println("Enter valid Operator");
+                return;
+
+        }
+        System.out.println(n1 + " " + operator + " " + n2 + " =" + output);
+    }
+
+}
