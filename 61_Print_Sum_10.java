d79c582f947673e40d68032842cb33e6eb274e11
Shriram Patil <shrirampatil246@gmail.com>
2022-11-01 10:37:15 +0530

    Add files via upload

---
+++ 61_Print_Sum_10.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sr = new Scanner(System.in);
+        int n = 10;
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
