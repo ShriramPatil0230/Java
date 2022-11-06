cd20a8ac45815cff6e565c7900b54cb4ca270287
Shriram Patil <shrirampatil246@gmail.com>
2022-11-06 09:35:45 +0530

    Update 68_printSum_average_largest_ElementofArray.java

--- 68_printSum_average_largest_ElementofArray.java
+++ 68_printSum_average_largest_ElementofArray.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sr = new Scanner(System.in);
         int n = sr.nextInt();
-        long arr[] = new long[n];
-        long sum = 0;
-        long average = 0;
-        long max = 0;
+        int arr[] = new int[n];
+        int sum = 0;
+        int average = 0;
+        int max = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = sr.nextInt();
         }
