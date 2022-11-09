3a71675fc01c036d7655559e084be53faa3c1dd1
Shriram Patil <shrirampatil246@gmail.com>
2022-11-09 16:59:11 +0530

    Update 02leapYear.java

--- 02leapYear.java
+++ 02leapYear.java
@@ -4,6 +4,7 @@ public class leapYear {
     public static void main(String[] args) {
         Scanner sr = new Scanner(System.in);
         int year = sr.nextInt();
+// Leap Year
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) // Check Leap year or not
         {
             System.out.println("Leap Year");
