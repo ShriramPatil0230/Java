7ecf8a81cf566ba232102c2137bd51564d3951bb
Shriram Patil <shrirampatil246@gmail.com>
2022-11-10 08:11:34 +0530

    Add files via upload

---
+++ 70_Lines_and_Stars.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sr = new Scanner(System.in);
+        int row = sr.nextInt();
+        int col = sr.nextInt();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
