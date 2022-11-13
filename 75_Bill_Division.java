0db7eda869acf408d8ba083fae5606a4979af529
Shriram Patil <shrirampatil246@gmail.com>
2022-11-13 08:48:29 +0530

    Update 75_Bill_Division.java

--- 75_Bill_Division.java
+++ 75_Bill_Division.java
@@ -9,10 +9,10 @@ You must determine if his calculation is correct.
 
 For example, assume the bill has the following prices: bill = [2, 4, 6], 
 Anna declines to eat item k = bill[2] which costs 6.
- If Brian calculates the bill correctly, Anna will pay (2+4)/2 = 3.
-  If he includes the cost of bill[2], he will calculate (2 + 4 + 6)/2 = 6.
-   In the second case, he should refund 3 to Anna.
-   Your code should print the difference of the share charged by Brian 
+If Brian calculates the bill correctly, Anna will pay (2+4)/2 = 3.
+If he includes the cost of bill[2], he will calculate (2 + 4 + 6)/2 = 6.
+In the second case, he should refund 3 to Anna.
+Your code should print the difference of the share charged by Brian 
 and the actual share of Anna. */
 public class Main {
     public static void main(String[] args) throws java.lang.Exception {
