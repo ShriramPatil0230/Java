2c97b59f8fa1219d4add6aa21465d3be3fc344a2
Shriram Patil <shrirampatil246@gmail.com>
2022-11-14 09:04:01 +0530

    Add files via upload

---
+++ 76_Decrease_Print_Increase_Print.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sr = new Scanner(System.in);
+        int n = sr.nextInt();
+        for (int i = n; i >= 1; --i) {
+            System.out.println(i);
+        }
+        for (int j = 1; j <= n; j++) {
+            System.out.println(j);
+        }
+    }
+}
