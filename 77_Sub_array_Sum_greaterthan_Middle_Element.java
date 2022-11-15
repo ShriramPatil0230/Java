4be2b478543751b62e8c6f1fc58ef212e8c514f5
Shriram Patil <shrirampatil246@gmail.com>
2022-11-15 08:43:19 +0530

    Add files via upload

---
+++ 77_Sub_array_Sum_greaterthan_Middle_Element.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int mid = arr[n / 2];
+        for (int i = 0; i < n; i++) {
+            int sum = 0; // store sum of subarray starting from index i
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum > mid) {
+                    // print the subarray
+                    for (int k = i; k <= j; k++) {
+                        System.out.print(arr[k] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+
+    }
+}
