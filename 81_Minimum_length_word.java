09606cecfb6883c6dba645ec0aa355f30c1b0b70
Shriram Patil <shrirampatil246@gmail.com>
2022-11-20 08:38:02 +0530

    Add files via upload

---
+++ 81_Minimum_length_word.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String a = in.nextLine();
+
+        int i = 0;
+        int j = 0;
+
+        int minimumLength = Integer.MAX_VALUE;
+
+        int indexOfWord = -1;
+
+        int l = a.length();
+        while (j < l && i < l) {
+            // keep incrementing j till when as soon as we find space or sentence is over
+            while (j < l && a.charAt(j) != ' ') {
+                j++;
+            }
+
+            int lengthOfWord = (j - i);
+            if (lengthOfWord < minimumLength) {
+                minimumLength = lengthOfWord;
+                indexOfWord = i;
+            }
+
+            j++;
+            i = j;
+        }
+
+        System.out.println(a.substring(indexOfWord, indexOfWord + minimumLength));
+    }
+}
