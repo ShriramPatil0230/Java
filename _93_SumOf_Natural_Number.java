efa51b0dfc520fe4e6a2878e8cd11bc88afac796
Shriram Patil <shrirampatil246@gmail.com>
2022-12-05 09:00:39 +0530

    Add files via upload

---
+++ _93_SumOf_Natural_Number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public long NumberSum(long N) {
+        if (N <= 1)
+            return N;
+        return N + NumberSum(N - 1);
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long N = scanner.nextLong();
+
+        Solution Obj = new Solution();
+        System.out.println(Obj.NumberSum(N));
+    }
+}
