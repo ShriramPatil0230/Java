2a78487e794a336bb255722c514b39df242ed582
Shriram Patil <shrirampatil246@gmail.com>
2022-12-09 09:16:19 +0530

    Add files via upload

---
+++ _98_Print_the_pattern.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void print(int n) {
+        if (n == 0)
+            return;
+        System.out.print("* ");
+        print(n - 1);
+    }
+
+    public static void solve(int n) {
+        if (n == 0)
+            return;
+        solve(n - 1);
+        print(n);
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        solve(n);
+
+    }
+}
